7c820a3fa215da6d46ff07b20197f4a1cbc8e458
kimlia.knopf@heidelberg.com <kimlia.knopf@heidelberg.com>
2026-03-10 11:34:35 +0100

    meinbereich

---
+++ src/main/java/algods/meine_bearbeitung/ElementTest.java
@@ -0,0 +1,52 @@
+package algods.meine_bearbeitung.vl_2026_03_10.Lists;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ElementTest {
+    @Test
+    public void testList_new() {
+        Element anchor = new Element();
+
+        assertEquals(0, anchor.data);
+        assertEquals(anchor, anchor.next);
+        assertEquals(anchor, anchor.prev);
+    }
+
+    @Test
+    public void testList_add_single() {
+        Element anchor = new Element();
+
+        anchor.add(42);
+
+        assertNotEquals(anchor, anchor.next);
+        assertNotEquals(anchor, anchor.prev);
+        assertEquals(0, anchor.data);
+        assertEquals(42, anchor.next.data);
+        assertEquals(anchor, anchor.next.next);
+        assertEquals(anchor, anchor.prev.prev);
+    }
+
+    @Test
+    public void testList_add_multiple() {
+        Element anchor = new Element();
+
+        anchor.add(42);
+        anchor.add(23);
+        anchor.add(38);
+
+        assertEquals(0, anchor.data);
+        assertEquals(42, anchor.next.data);
+        assertEquals(23, anchor.next.next.data);
+        assertEquals(38, anchor.next.next.next.data);
+        assertEquals(anchor, anchor.next.next.next.next);
+
+        assertEquals(0, anchor.data);
+        assertEquals(38, anchor.prev.data);
+        assertEquals(23, anchor.prev.prev.data);
+        assertEquals(42, anchor.prev.prev.prev.data);
+        assertEquals(anchor, anchor.prev.prev.prev.prev);
+    }
+}
